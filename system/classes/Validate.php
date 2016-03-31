865f763cc640c21fea59103901f861a511d6df25
Josiah Bubna <joebubna@gmail.com>
2016-03-31 00:21:10 -0700

    Made checks pass by returning true, added ability to define custom checks.

--- system/classes/Validate.php
+++ system/classes/Validate.php
@@ -4,9 +4,11 @@ namespace Library;
 
 class Validate
 {
-    private $controller;    // Stores a reference to the calling controller.
-    private $errors;        // Array of validation errors.
-    private $data;          // The data array to be validated. Defaults to $_POST.
+    protected $controller;    // Stores a reference to the calling controller.
+    protected $errors;        // Array of validation errors.
+    protected $data;          // The data array to be validated. Defaults to $_POST.
+    protected $lang;          // For holding error strings.
+    protected $customChecks;  // For holding custom check definitions.
     
     // Some things to validate?
     // required
@@ -31,9 +33,11 @@ class Validate
         // Init stuff.
         $this->errors = array();
         $this->lang = new \stdClass();
+        $this->customChecks = new \stdClass();
         
         // Setting this up for future language localization.
         $this->lang->required = '%s is required!';
+        $this->lang->valid_email = "Valid %s required";
     }
     
     
@@ -50,6 +54,16 @@ class Validate
             $this->controller->setData('errors', $this->errors);
             return false;
         }
+    }
+    
+    
+    /**
+     *  Stores a customly defined validation check.
+     */
+    public function def ($checkName, $class, $method, $errorMessage, $passing = true)
+    {
+        $this->customChecks->$checkName = ['_call', $class, $method, $passing];
+        $this->lang->$checkName = $errorMessage;
     }
     
     
@@ -75,31 +89,37 @@ class Validate
         // Run checks
         foreach ($checks as $check) {
             
-            if (is_array($check)) {
+            $checkName = $check;
+            
+            if (isset($this->customChecks->$check)) {
+                
+                // Grab this custom check's definition.
+                $customCheckDef = $this->customChecks->$check;
 
                 // Grab custom check type. Ex. "call"
-                $custom = '_'.$check[0];
+                //$checkName = $check;
+                $customType = $customCheckDef[0];
                 
-                // Remove the check type from array.
-                array_shift($check);
-                
-                // Add the fieldData to the front of the array
-                array_unshift($check, $fieldData);
+                // Define the arguments that will be passed to the custom check.
+                $arguments = array($fieldData, $customCheckDef[1], $customCheckDef[2], $customCheckDef[3]);
                 
                 // Call the custom check.
-                $checkResult = call_user_func_array(array($this, $custom), $check);
+                $checkResult = call_user_func_array(array($this, $customType), $arguments);
             }
             else {
+                // Set this check's name. I.e. 'required'
+                //$checkName = $check;
+                
                 // Append underscore to the check method's name.
-                $check = '_'.$check;
+                $check = '_'.$checkName;
                 
                 // Call a built-in check that's part of this Validation class.
-                $checkResult = $this->$check($fieldData, $humanName);
+                $checkResult = $this->$check($fieldData);
             }
             
-            // If the result of the called check is anything other than FALSE, set a validation error.
-            if ($checkResult) {
-                $this->errors[] = $checkResult;
+            // If the result of the called check is anything other than true, set a validation error.
+            if ($checkResult == false) {
+                $this->errors[] =  sprintf($this->lang->$checkName, $humanName);
                 $checkFailures++;
             }
         }
@@ -107,6 +127,95 @@ class Validate
         // After all checks for this data field have been run, return if Validation passed (TRUE) or failed (FALSE);
         return $checkFailures == 0 ? true : false;
     }
+        
+    
+    
+    ////////////////////////////////////////////////
+    //
+    //  The Validation Methods
+    //
+    ////////////////////////////////////////////////
+    
+    
+    /**
+     *  Form field is required.
+     */
+    protected function _required($fieldData)
+    {
+        if ($fieldData)
+            return true;
+        else
+            return false;
+    }
+    
+    
+    /**
+     *  This handles when a user wants to call a custom validation method in some other class.
+     *  A common example would be checking if a username is already taken in a database
+     *  when using a user register form.
+     *
+     *  $fieldData is the actual data.
+     *  $controller is the controller that needs to be invoked for this custom check.
+     *  $method is the method that needs to be called.
+     *  $passing defines what the method should return to pass the test. Two Examples below:
+     *          $user->nameExists($fieldData) you would want to return FALSE. So set $passing = false.
+     *          $user->nameAvailable($fieldData) you would want to return TRUE. So set $passing = true.
+     */
+    protected function _call($fieldData, $controller, $method, $passing)
+    {      
+        // If data to be passed to the method isn't an array, put it in array format.
+        if (!is_array($fieldData))
+            $fieldData = array($fieldData);
+        
+        // Call custom controller->method and pass the data to it.
+        $result = call_user_func_array(array($controller, $method), $fieldData);
+        
+        // If the returned result meets expections, pass test (return false) otherwise return message.
+        return $result == $passing ? true : false;
+    }
+    
+    
+    /**
+     *  Just trims the field. This check Never fails.
+     */
+    protected function _trim(&$fieldData)
+    {
+        $fieldData = trim($fieldData);
+        return true;
+    }
+
+    // valid_email
+    protected function _valid_email($fieldData, $humanName)
+    {
+        echo $fieldData;
+        if (preg_match('/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/', $fieldData)) {
+            return true;
+        }
+        return false;
+    }
+
+    // mathces
+    protected function _matches($fieldData, $humanName, $array)
+    {
+        var_dump($array);
+    }
+
+
+    // matches[password]
+    // min_length[5]
+    // max_length[12]
+    // PHP single arg functions like: htmlspecialchars, trim
+    // Custom functions
+    
+    
+    
+    
+    ////////////////////////////////////////////////
+    //
+    //  Form Utility Methods
+    //
+    ////////////////////////////////////////////////
+    
     
     /**
      *  For resetting a form field's data after a failed validation.
@@ -175,84 +284,5 @@ class Validate
             }
         }
     }
-    
-    
-    ////////////////////////////////////////////////
-    //
-    //  The Validation Methods
-    //
-    ////////////////////////////////////////////////
-    
-    
-    /**
-     *  Form field is required.
-     */
-    protected function _required($fieldData, $humanName)
-    {
-        if ($fieldData)
-            return false;
-        else
-            return sprintf($this->lang->required, $humanName);
-    }
-    
-    
-    /**
-     *  This handles when a user wants to call a custom validation method in some other class.
-     *  A common example would be checking if a username is already taken in a database
-     *  when using a user register form.
-     *
-     *  $fieldData is the actual data.
-     *  $controller is the controller that needs to be invoked for this custom check.
-     *  $method is the method that needs to be called.
-     *  $passing defines what the method should return to pass the test. Two Examples below:
-     *          $user->nameExists($fieldData) you would want to return FALSE. So set $passing = false.
-     *          $user->nameAvailable($fieldData) you would want to return TRUE. So set $passing = true.
-     *  $message is the custom error message to display if the check fails.
-     */
-    protected function _call($fieldData, $controller, $method, $passing, $message)
-    {      
-        // If data to be passed to the method isn't an array, put it in array format.
-        if (!is_array($fieldData))
-            $fieldData = array($fieldData);
-        
-        // Call custom controller->method and pass the data to it.
-        $result = call_user_func_array(array($controller, $method), $fieldData);
-        
-        // If the returned result meets expections, pass test (return false) otherwise return message.
-        return $result == $passing ? false : $message;
-    }
-    
-    
-    /**
-     *  Just trims the field. This check Never fails.
-     */
-    protected function _trim(&$fieldData)
-    {
-        $fieldData = trim($fieldData);
-        return false;
-    }
-
-    // valid_email
-    protected function _valid_email($fieldData, $humanName)
-    {
-        echo $fieldData;
-        if (preg_match('/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/', $fieldData)) {
-            return false;
-        }
-        return "Valid $humanName required";
-    }
-
-    // mathces
-    protected function _matches($fieldData, $humanName, $array)
-    {
-        var_dump($array);
-    }
-
-
-    // matches[password]
-    // min_length[5]
-    // max_length[12]
-    // PHP single arg functions like: htmlspecialchars, trim
-    // Custom functions
     
 }
