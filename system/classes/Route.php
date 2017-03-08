386089bcd4de00efd4d6c547dd3afad17c2a9947
Josiah Bubna <josiahbubna@FM-JOSBUB-MAC.local>
2017-03-07 16:25:14 -0800

    Added additional method to Route.

--- system/classes/Route.php
+++ system/classes/Route.php
@@ -3,7 +3,7 @@ namespace Cora;
 
 class Route extends Framework
 {
-    
+
     protected $container;           // VARIES   - For passing data to controllers.
     protected $pathString;          // STRING   - URL excluding siteURL. mysite.com/FROM/HERE/ONWARD/
     protected $path;                // ARRAY    - Array version of pathString. URL pieces after baseURL.
@@ -18,12 +18,12 @@ class Route extends Framework
     protected $collectionID;        // INT      - The ID if a collection ID is specified.
 
     protected $paths;               // ARRAY    - Holds custom defined paths.
-    
-    
+
+
     public function __construct($container = false)
     {
         parent::__construct(); // Call parent constructor too so we don't lose functionality.
-        
+
         // For site specific data. This will be passed to Cora's controllers when they
         // are invoked in the routeExec() method.
         $this->container = $container;
@@ -34,7 +34,7 @@ class Route extends Framework
             include($this->config['basedir'].'cora/app/Paths.php');
         }
         $this->paths = $paths;
-        
+
         // Namespacing Defaults
         $this->controllerNamespace = 'Controllers\\';
 
@@ -43,10 +43,23 @@ class Route extends Framework
 
     public function run($uri = false, $method = false)
     {
-        // Set Request type. 
+        // Setup and check for route.
+        $this->routeProcess($uri, $method);
+
+        // Debug
+        $this->debug('Route: ' . $this->pathString);
+
+        // Execute route (will load 404 if not exists)
+        $this->routeExec();
+    }
+
+
+    public function routeProcess($uri = false, $method = false)
+    {
+        // Set Request type.
         if (!$method) $method = $_SERVER['REQUEST_METHOD'];
         $this->httpMethod = $method;
-        
+
         // Set PATH info.
         if (!$uri) $uri = $_SERVER['REQUEST_URI'];
         $this->setPath($uri);
@@ -54,39 +67,34 @@ class Route extends Framework
         if (!$this->customFind()) {
             $this->routeFind();
         }
-
-        // Debug
-        $this->debug('Route: ' . $this->pathString);
-
-        $this->routeExec();
     }
 
 
     public function customFind()
     {
-        // Setup 
+        // Setup
         $url = $this->pathString;
         $matchFound = false;
         $templateRegex = "/\{\w+}/";
-        
+
         foreach ($this->paths as $path) {
-            
+
             ///////////////////////////////////////////////
             // Grab path URL template variables
             ///////////////////////////////////////////////
             /**
-             *  Create an array of path variables from custom path URL definition. 
+             *  Create an array of path variables from custom path URL definition.
              *  I.E. users/{someVariable}/{anotherVariable}
              */
             $templateVariables = [];
             preg_match_all($templateRegex, $path->url, $templateVariables);
-            
+
             ///////////////////////////////////////////////
-            // Replacing templates variables with regex 
+            // Replacing templates variables with regex
             ///////////////////////////////////////////////
-            /** 
-            *  This is for replacing the bracket variables in the custom route with a regular expression string. 
-            *  If no custom definition for the variable was set in the variable definitions section, then the variable 
+            /**
+            *  This is for replacing the bracket variables in the custom route with a regular expression string.
+            *  If no custom definition for the variable was set in the variable definitions section, then the variable
             *  will default to alpha-numeric with underscore and dash.
             *  INPUT   = users/action-subaction/23
             *  OUTPUT  = users\/([a-zA-Z0-9-_]+)\-([a-zA-Z0-9-_]+)\/([0-9]+)
@@ -104,16 +112,16 @@ class Route extends Framework
             ///////////////////////////////////////////////
             // Check for regex match against URL given
             ///////////////////////////////////////////////
-            /** 
-            *   This takes the current URL and checks if it matches the custom route we are currently looking at. 
-            *   If there's a match, then it marks that we found a valid custom path. 
-            *   $urlData will get populated with the matching variable values from the URL. This works because 
+            /**
+            *   This takes the current URL and checks if it matches the custom route we are currently looking at.
+            *   If there's a match, then it marks that we found a valid custom path.
+            *   $urlData will get populated with the matching variable values from the URL. This works because
             *   all the regexes from the above section are within parenthesis.
             *   With the following regex: "users/([a-zA-Z0-9_]+)/([a-zA-Z0-9_]+)"
-            *   The match for the first set of parenthesis will get placed in $urlData[1], the 2nd set in $urlData[2], etc. 
-            *   
+            *   The match for the first set of parenthesis will get placed in $urlData[1], the 2nd set in $urlData[2], etc.
+            *
             *   See below for an example:
-            *   if URL              = articles/grab-all/23 
+            *   if URL              = articles/grab-all/23
             *   and custom route    = articles/{action}-{modifier}/{id}
             *   $urlData[1] will be 'grab', $urlData[2] will be 'all', and $urlData[3] will be 23.
             */
@@ -122,17 +130,17 @@ class Route extends Framework
             if (preg_match("/$urlRegex/", $url, $urlData)) {
                 $matchFound = true;
 
-                // A match was found, so at this point $urlData contains the matching pieces from the regex. 
-                // With the first variable match being in $urlData[1]. Offset 0 is useless, so let's unset it so it 
+                // A match was found, so at this point $urlData contains the matching pieces from the regex.
+                // With the first variable match being in $urlData[1]. Offset 0 is useless, so let's unset it so it
                 // does screw up our count.
                 unset($urlData[0]);
                 for($i=1; $i<=count($urlData); $i++) {
-                    // For each REAL variable value from our URL, let's replace any references to that variable in our 
-                    // route with the actual values. 
-                    // Example: 
+                    // For each REAL variable value from our URL, let's replace any references to that variable in our
+                    // route with the actual values.
+                    // Example:
                     //                     Path URL = "users/{action}-{modifier}"
-                    //    Actual URL in web browser = "users/fetch-all" 
-                    //                   Path Route = "users/{action}/{modifier}" 
+                    //    Actual URL in web browser = "users/fetch-all"
+                    //                   Path Route = "users/{action}/{modifier}"
                     //  Final Route after this loop = "users/fetch/all"
                     $pathVar = $templateVariables[0][$i-1]; // E.g. {action}. $templateVariables starts at offset 0.
                     $pathVarValue = $urlData[$i];
@@ -145,14 +153,14 @@ class Route extends Framework
             // Handle Path match
             ///////////////////////////////////////////////
             /**
-             *  If this iteration of the loop found a match to a custom path, 
-             *  Then execute that Path's pre execution function and if that returns true, 
+             *  If this iteration of the loop found a match to a custom path,
+             *  Then execute that Path's pre execution function and if that returns true,
              *  then set our new Route to be the one defined in the custom Path.
              */
             if ($matchFound) {
                 // If the path accepts all http method types or the current request type is listed as being accepted.
                 if (
-                    stripos($path->actions, 'all') !== false || 
+                    stripos($path->actions, 'all') !== false ||
                     stripos($path->actions, $this->httpMethod) !== false
                 ) {
                     if (!$path->preExecCheck()) {
@@ -160,26 +168,26 @@ class Route extends Framework
                         exit;
                     }
 
-                    if ($path->route) {         
+                    if ($path->route) {
                         $this->setPath($finalRoute);
                     }
-                    
+
                     $this->routeFind();
                     return true;
                 }
-            } 
+            }
         }
         return false;
     }
-    
-    
+
+
     /**
      *  Given a URL, sets needed PATH information.
      *
      *  NOTE: $_SERVER['QUERY_STRING'] is set by the server and may not be available
      *  depending on the server being used.
      */
-    public function setPath($url) 
+    public function setPath($url)
     {
         // Removes any GET data from the url.
         // Makes 'mysite.com/websites/build/?somevalue=test' INTO '/websites/build'
@@ -188,17 +196,17 @@ class Route extends Framework
         // Removes the 'mysite.com' from 'mysite.com/controller/method/id/'
         // Resulting pathString will be 'controller/method/id/'
         $this->pathString = explode($this->config['site_url'], $cleanURI, 2)[1];
-        
+
         // If config option to make url lowercase is true
         if ($this->config['lowercase_url']) {
             $this->pathString = strtolower($this->pathString);
         }
-        
+
         // Setup Path array
         $this->path = explode('/', $this->pathString);
     }
-    
-    
+
+
     /**
      *  Searches through $path to figure out what part of it is the controller.
      *  This requires searching through the filesystem.
@@ -212,12 +220,12 @@ class Route extends Framework
      public function routeFind($basePath = '', $offset = 0)
     {
         $this->debug('');
-        
+
         // Vars setup
         $curPath = $this->partialPathArray($offset, 1);
         $controller = '';
         $controllerFileName = '';
-        
+
         $this->debug('Current Path: '.implode('/', $curPath));
 
         // if $curPath isn't empty
@@ -233,7 +241,7 @@ class Route extends Framework
                                     $controller .
                                     $this->config['controllersPostfix'] .
                                     '.php';
-        
+
         // Set working filepath.
         $dirpath = $this->config['pathToControllers'].$basePath.$controller_path;
         $filepath = $this->config['pathToControllers'].$basePath.$controllerFileName;
@@ -246,28 +254,28 @@ class Route extends Framework
             $this->controllerPath = $this->partialPathString(0, $offset+1);
             $this->controllerOffset = $offset;
             $this->controllerName = $controller;
-            
+
             $this->debug('File Found: ' . $controllerFileName);
             $this->debug('Controller Path: ' . $this->controllerPath);
-            
+
             // Create instance of controller and check if that method exists in it. Otherwise we want to keep
             // looking for other sub-folder classes that might map to the url.
             $controllerInstance = $this->getController();
             $method = $this->getMethod();
             $this->debug("getMethod() call in routeFind() returns: ".$method);
-            
+
             if (!is_callable(array($controllerInstance, $method))) {
                 // Update namespace
                 $this->controllerNamespace .= $this->controllerName.'\\';
-                
+
                 //  Controller->Method combo doesn't exist, so undo previously set data.
                 $this->controllerPath = null;
                 $this->controllerOffset = null;
                 $this->controllerName = null;
-                
+
                 // Debug
                 $this->debug('Not matching method within controller. Continuing search.');
-                
+
             } else {
                 // Valid controller+method combination found, so stop searching further.
                 $this->controller   = $controllerInstance;
@@ -283,21 +291,21 @@ class Route extends Framework
         // Else check if there's a matching directory we can look through.
         if (is_dir($dirpath)) {
             $this->debug('Directory Found: ' . $basePath . $controller);
-            
+
             // Recursive call
             $this->routeFind($basePath . $controller_path . '/', $offset+1);
         }
-        
+
     } // end routeFind
-    
-    
+
+
     /**
      *  Uses the info generated by routeFind() to then create an instance of the
      *  appropriate controller and call the desired method.
      */
     public function routeExec()
     {
-        
+
         // If no controller was found by routeFind()...
         if (!isset($this->controllerPath)) {
             $this->debug('routeExec: No controllerPath set. Routing to 404.');
@@ -312,13 +320,13 @@ class Route extends Framework
         else {
             $methodArgs = $this->partialPathArray($this->controllerOffset+2);
         }
-        
+
         // Remove the last item from arguments if empty.
         $lastItem = count($methodArgs)-1;
         if (isset($methodArgs[$lastItem]) && $methodArgs[$lastItem] == '') {
             array_pop($methodArgs);
         }
-        
+
         // If no arguments are set then make empty array.
         if (empty($methodArgs) || $methodArgs[0] == '') {
             $methodArgs = array();
@@ -342,12 +350,12 @@ class Route extends Framework
          *  $b will have the value 'Param2'  ... and so forth.
          */
         call_user_func_array(array($this->controller, $this->method), $methodArgs);
-        
+
     } // end routeExec
-    
-    
+
+
     /**
-     *  Checks if routeFind found a path to a controller. Can optionally check this 
+     *  Checks if routeFind found a path to a controller. Can optionally check this
      *  before calling routeExec.
      *
      *  Note: This helps with integrating Cora into legacy applications.
@@ -356,19 +364,21 @@ class Route extends Framework
      */
     public function exists()
     {
+
+
         if (!isset($this->controllerPath)) {
             return false;
         }
         return true;
     }
-    
-    
+
+
     protected function getController()
     {
-        
+
         // Load generic Cora parent class
         require_once('Cora.php');
-        
+
         // Include the controller code.
         $cPath =    $this->config['pathToControllers'] .
                     $this->getPath($this->controllerPath) .
@@ -376,29 +386,29 @@ class Route extends Framework
                     $this->controllerName .
                     $this->config['controllersPostfix'] .
                     '.php';
-        
+
         if (file_exists($cPath)) {
             include_once($cPath);
         }
         // Return an instance of the controller.
         $class = $this->controllerNamespace.$this->getClassName($this->controllerName);
-        
+
         return new $class($this->container);
     }
-    
-    
+
+
     protected function getMethod()
     {
-        
+
         $method = '';
         $this->collectionIDGiven = false;
         $this->collectionID = false;
-        
+
         // Figure out method to be called, or use default.
         if (isset($this->path[$this->controllerOffset+1])) {
             $method = $this->path[$this->controllerOffset+1];
-            
-            // If no method is specified or if the method spot is numeric (indicating 
+
+            // If no method is specified or if the method spot is numeric (indicating
             // that an ID is being passed to a collection for RESTful routing)
             // Route to default method.
             if ($method == '') {
@@ -407,19 +417,19 @@ class Route extends Framework
             else if (is_numeric($method)) {
                 $this->collectionIDGiven = true;
                 $this->collectionID = $method;
-                $method = $this->config['default_method'];          
+                $method = $this->config['default_method'];
             }
         }
         else {
             $method = $this->config['default_method'];
         }
-        
+
         // RESTful routing:
         // Modify method routed to if request is not of type GET.
         if ($this->config['enable_RESTful']) {
 
             $httpMethod = $_SERVER['REQUEST_METHOD'];
-            
+
             if ($this->collectionIDGiven) {
                 switch ($httpMethod) {
                     case 'GET':
@@ -450,17 +460,17 @@ class Route extends Framework
                 }
             }
         }
-        
+
         return $method;
-        
+
     } // END getMethod
-    
+
 
     protected function getClassName($slug)
     {
         return str_replace(' ', '', ucwords(preg_replace('/[-_]/', ' ', $slug)));
     }
-    
+
     /**
      *  The following two methods work off the URL path stored in $this->path.
      *  They are used to return part of that path in either Array or String form
@@ -474,7 +484,7 @@ class Route extends Framework
         $partialPathArray = array_slice($dataArray, $offset, $length);
         return implode('/', $partialPathArray);
     }
-       
+
     protected function partialPathArray($offset, $length = null)
     {
         return array_slice($this->path, $offset, $length);
@@ -482,20 +492,20 @@ class Route extends Framework
 
     /**
      *  Sample Types:
-     *  401 = Access Denied 
+     *  401 = Access Denied
      *  404 = Not Found
-     */    
+     */
     protected function error($type)
     {
         $filepath = $this->config['basedir'].'cora/app/Error.php';
-            
+
         if (file_exists($filepath)) {
             $error = new \Cora\App\Error($this->container);
         }
         else {
             $error = new \Cora\Error($this->container);
         }
-        $error->handle($type);    
+        $error->handle($type);
     }
-    
+
 } // end Class
